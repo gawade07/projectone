b6470329d10ab2ff7fb5ec891139384c8c25c834
gawade07 <ketangawade111@gmail.com>
2022-11-11 21:46:57 +0530

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageComponent } from './page/page.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    PageComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
